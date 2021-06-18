a08ac3719832426b50a1df37dd1dbfa68f902c9c
YuxLiu <liuyuxuan1230@outlook.com>
2021-06-18 16:36:27 +0800

    [sign_up]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
