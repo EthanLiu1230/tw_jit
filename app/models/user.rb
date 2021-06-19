a425ba572d5da46749d4552bf33a23fbf1935750
YuxLiu <liuyuxuan1230@outlook.com>
2021-06-19 13:30:28 +0800

    [article.author.name column] # all basic features covered

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :articles, foreign_key: 'author_id', dependent: :delete_all
+
+  def name
+    email.split('@')[0].capitalize
+  end
 end
